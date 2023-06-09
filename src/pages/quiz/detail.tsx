87a8908f0167193845198da42c62c021e0c1362b
reyys <reyyy0207@gmail.com>
2023-06-09 10:15:03 +0700

    fix bug quiz

--- src/pages/quiz/detail.tsx
+++ src/pages/quiz/detail.tsx
@@ -18,6 +18,44 @@ import DeleteQuiz from '@/components/popup/delete_quiz';
 import ExportQuiz from '@/components/popup/export_quiz';
 
 const QuizDetailPage: NextPage = () => {
+  const quizData = [
+    {
+      key: 'question1',
+      question: 'What is the name of the company that sent the invoice? 1',
+      options: [
+        'Seed Agency',
+        'Dylan Williams',
+        'Mentorclass Corporation',
+        "Wise's Address",
+      ],
+      explanation: `The name of the company that sent the invoice is "Mentroclass Corporation"`,
+      quote: 'Mentorclass Corporation',
+    },
+    {
+      key: 'question2',
+      question: 'What is the name of the company that sent the invoice? 2',
+      options: [
+        'Seed Agency',
+        'Dylan Williams',
+        'Mentorclass Corporation',
+        "Wise's Address",
+      ],
+      explanation: `The name of the company that sent the invoice is "Mentroclass Corporation"`,
+      quote: 'Mentorclass Corporation',
+    },
+    {
+      key: 'question3',
+      question: 'What is the name of the company that sent the invoice?',
+      options: [
+        'Seed Agency',
+        'Dylan Williams',
+        'Mentorclass Corporation',
+        "Wise's Address",
+      ],
+      explanation: `The name of the company that sent the invoice is "Mentroclass Corporation"`,
+      quote: 'Mentorclass Corporation',
+    },
+  ];
   const [quiz, setQuiz] = useState(quizData);
   const [answers, setAnswers] = useState<string[]>([
     'Dylan Williams',
@@ -107,133 +145,132 @@ const QuizDetailPage: NextPage = () => {
                     {...provided.droppableProps}
                     ref={provided.innerRef}
                   >
-                    {quiz.length > 0 &&
-                      quiz.map((x, index) => {
-                        return (
-                          <Draggable
-                            key={`draggable-${index}`}
-                            draggableId={`draggable-${index}`}
-                            index={index}
-                            isDragDisabled={!isGrDragIconHovered}
-                          >
-                            {(provided2) => (
-                              <li
-                                ref={provided2.innerRef}
-                                {...provided2.draggableProps}
-                                {...provided2.dragHandleProps}
-                                className='relative mb-12'
-                              >
-                                <GrDrag
-                                  onMouseEnter={() =>
-                                    setIsGrDragIconHovered(true)
-                                  }
-                                  onMouseLeave={() =>
-                                    setIsGrDragIconHovered(false)
-                                  }
-                                  className='left-[-5%] top-[2.5%] z-[99] mb-5 text-[1.5rem] xl:absolute xl:mb-0'
-                                />
-                                <div className='rounded-md border bg-white p-5 drop-shadow-md'>
-                                  <div className='flex justify-between space-x-2'>
-                                    <div className='flex w-full space-x-2'>
-                                      <div>{index + 1}.</div>
-                                      <div
-                                        contentEditable
-                                        className='w-full outline-none'
-                                        id='question'
-                                        onChange={(e) => inputHandler(index, e)}
-                                      >
-                                        {x.question}
-                                      </div>
+                    {quiz.map((x, index) => {
+                      return (
+                        <Draggable
+                          key={`draggable-${index}`}
+                          draggableId={`draggable-${index}`}
+                          index={index}
+                          isDragDisabled={!isGrDragIconHovered}
+                        >
+                          {(provided2) => (
+                            <li
+                              ref={provided2.innerRef}
+                              {...provided2.draggableProps}
+                              {...provided2.dragHandleProps}
+                              className='relative mb-12'
+                            >
+                              <GrDrag
+                                onMouseEnter={() =>
+                                  setIsGrDragIconHovered(true)
+                                }
+                                onMouseLeave={() =>
+                                  setIsGrDragIconHovered(false)
+                                }
+                                className='left-[-5%] top-[2.5%] z-[99] mb-5 text-[1.5rem] xl:absolute xl:mb-0'
+                              />
+                              <div className='rounded-md border bg-white p-5 drop-shadow-md'>
+                                <div className='flex justify-between space-x-2'>
+                                  <div className='flex w-full space-x-2'>
+                                    <div>{index + 1}.</div>
+                                    <div
+                                      contentEditable
+                                      className='w-full outline-none'
+                                      id='question'
+                                      onChange={(e) => inputHandler(index, e)}
+                                    >
+                                      {x.question}
                                     </div>
-                                    <FiXCircle
-                                      className='text-[1.25rem]'
-                                      onClick={() => setDeleteModal(true)}
-                                    />
                                   </div>
-                                  <div className='mt-3'>
-                                    {x.options.map((option, order) => {
-                                      const optionKey = `${index}-${order}`;
-                                      return (
-                                        <div
-                                          key={optionKey}
-                                          className={`mt-1 rounded-xl p-2 ${
-                                            answers[index] === option &&
-                                            'bg-purple-100'
-                                          } transition-all`}
-                                        >
-                                          <div className='flex cursor-pointer items-center space-x-3 rounded-full border bg-white px-4 py-1 lg:w-[300px]'>
-                                            <div
-                                              onClick={() =>
-                                                setAnswers((prevState) => {
-                                                  prevState[index] = option;
-                                                  return [...prevState];
-                                                })
-                                              }
-                                              className={`h-[15px] w-[15px] rounded-full border ${
-                                                answers[index] === option
-                                                  ? 'bg-primary'
-                                                  : 'bg-gray-200'
-                                              }`}
-                                            ></div>
-                                            <div
-                                              contentEditable
-                                              id='options'
-                                              onChange={(e) =>
-                                                inputHandler(index, e, order)
-                                              }
-                                              className='w-full font-light outline-none'
-                                            >
-                                              {option}
-                                            </div>
+                                  <FiXCircle
+                                    className='text-[1.25rem]'
+                                    onClick={() => setDeleteModal(true)}
+                                  />
+                                </div>
+                                <div className='mt-3'>
+                                  {x.options.map((option, order) => {
+                                    const optionKey = `${index}-${order}`;
+                                    return (
+                                      <div
+                                        key={optionKey}
+                                        className={`mt-1 rounded-xl p-2 ${
+                                          answers[index] === option &&
+                                          'bg-purple-100'
+                                        } transition-all`}
+                                      >
+                                        <div className='flex cursor-pointer items-center space-x-3 rounded-full border bg-white px-4 py-1 lg:w-[300px]'>
+                                          <div
+                                            onClick={() =>
+                                              setAnswers((prevState) => {
+                                                prevState[index] = option;
+                                                return [...prevState];
+                                              })
+                                            }
+                                            className={`h-[15px] w-[15px] rounded-full border ${
+                                              answers[index] === option
+                                                ? 'bg-primary'
+                                                : 'bg-gray-200'
+                                            }`}
+                                          ></div>
+                                          <div
+                                            contentEditable
+                                            id='options'
+                                            onChange={(e) =>
+                                              inputHandler(index, e, order)
+                                            }
+                                            className='w-full font-light outline-none'
+                                          >
+                                            {option}
                                           </div>
                                         </div>
-                                      );
-                                    })}
-                                  </div>
+                                      </div>
+                                    );
+                                  })}
                                 </div>
-                                {/* Answer  */}
+                              </div>
+                              {/* Answer  */}
 
-                                <div
-                                  className={`${
-                                    !showExplanation
-                                      ? 'max-h-0 overflow-hidden'
-                                      : 'mt-5 max-h-[20rem]'
-                                  } transition-all`}
-                                >
-                                  <div className='rounded-md border bg-white p-5 drop-shadow-lg'>
-                                    <div className='flex flex-col xl:flex-row xl:space-x-2'>
-                                      <span className='font-medium'>
-                                        Explanation:
-                                      </span>{' '}
-                                      <div
-                                        contentEditable
-                                        id='explanation'
-                                        onChange={(e) => inputHandler(index, e)}
-                                        className='w-full outline-none'
-                                      >
-                                        {x.explanation}
-                                      </div>
+                              <div
+                                className={`${
+                                  !showExplanation
+                                    ? 'max-h-0 overflow-hidden'
+                                    : 'mt-5 max-h-[20rem]'
+                                } transition-all`}
+                              >
+                                <div className='rounded-md border bg-white p-5 drop-shadow-lg'>
+                                  <div className='flex flex-col xl:flex-row xl:space-x-2'>
+                                    <span className='font-medium'>
+                                      Explanation:
+                                    </span>{' '}
+                                    <div
+                                      contentEditable
+                                      id='explanation'
+                                      onChange={(e) => inputHandler(index, e)}
+                                      className='w-full outline-none'
+                                    >
+                                      {x.explanation}
                                     </div>
-                                    <div className='mt-5 flex flex-col xl:mt-0 xl:flex-row xl:space-x-2'>
-                                      <span className='font-medium md:whitespace-nowrap'>
-                                        Quote from provided content:
-                                      </span>
-                                      <div
-                                        id='quote'
-                                        contentEditable
-                                        onChange={(e) => inputHandler(index, e)}
-                                        className='w-full outline-none'
-                                      >
-                                        {x.quote}
-                                      </div>
+                                  </div>
+                                  <div className='mt-5 flex flex-col xl:mt-0 xl:flex-row xl:space-x-2'>
+                                    <span className='font-medium md:whitespace-nowrap'>
+                                      Quote from provided content:
+                                    </span>
+                                    <div
+                                      id='quote'
+                                      contentEditable
+                                      onChange={(e) => inputHandler(index, e)}
+                                      className='w-full outline-none'
+                                    >
+                                      {x.quote}
                                     </div>
                                   </div>
                                 </div>
-                              </li>
-                            )}
-                          </Draggable>
-                        );
-                      })}
+                              </div>
+                            </li>
+                          )}
+                        </Draggable>
+                      );
+                    })}
                     {provided.placeholder}
                   </ul>
                 )}
@@ -245,45 +282,6 @@ const QuizDetailPage: NextPage = () => {
     </>
   );
 };
-
-const quizData = [
-  {
-    key: 'question1',
-    question: 'What is the name of the company that sent the invoice?',
-    options: [
-      'Seed Agency',
-      'Dylan Williams',
-      'Mentorclass Corporation',
-      "Wise's Address",
-    ],
-    explanation: `The name of the company that sent the invoice is "Mentroclass Corporation"`,
-    quote: 'Mentorclass Corporation',
-  },
-  {
-    key: 'question2',
-    question: 'What is the name of the company that sent the invoice?',
-    options: [
-      'Seed Agency',
-      'Dylan Williams',
-      'Mentorclass Corporation',
-      "Wise's Address",
-    ],
-    explanation: `The name of the company that sent the invoice is "Mentroclass Corporation"`,
-    quote: 'Mentorclass Corporation',
-  },
-  {
-    key: 'question3',
-    question: 'What is the name of the company that sent the invoice?',
-    options: [
-      'Seed Agency',
-      'Dylan Williams',
-      'Mentorclass Corporation',
-      "Wise's Address",
-    ],
-    explanation: `The name of the company that sent the invoice is "Mentroclass Corporation"`,
-    quote: 'Mentorclass Corporation',
-  },
-];
 
 export const getServerSideProps: GetServerSideProps = async () => {
   resetServerContext(); // <-- CALL RESET SERVER CONTEXT, SERVER SIDE
